deeae32f2e288ffd6a19fc75eda31616e14e65d0
YanCarlosCamargo <yancarlos55@yahoo.com.br>
2022-03-15 12:23:32 +0000

    Adicionado titulo e icone de titulo. Não é o icone final mas já saimos daquele icone do react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import Header from './components/Header'
 import Hero from './components/Hero';
 import RegularSection from './components/RegularSection';
-
-import './App.css'
+import { Image } from 'react';
+import './App.css';
 import Footer from './components/Footer';
 
 
