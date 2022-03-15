bffe5a98185ec43f4fb6a3be22a2e479affbcdfe
YanMCoutinho <yan.m.coutinho@gmail.com>
2022-03-15 10:32:43 -0300

    Changed Header and Footer to be imported in LandingPage besides App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,4 @@
-import Header from './components/Header'
-import Hero from './components/Hero';
-import RegularSection from './components/RegularSection';
-
 import './App.css'
-import Footer from './components/Footer';
 import LandingPage from './components/LandingPage';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
@@ -11,16 +6,12 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      <Header />
-
         <BrowserRouter>
           <Routes>
             <Route path='/' element={ <LandingPage /> } />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </BrowserRouter>
-        
-      <Footer />
     </div>
   );
 }
