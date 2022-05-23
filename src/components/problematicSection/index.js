329ffacd6105c3d4f6691b4932737fa23428f30e
Victor Lira <victor.liracarlos@gmail.com>
2022-05-23 09:19:23 -0300

    WIP

---
+++ src/components/problematicSection/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function problematicSection() {
+    <section className='problematic-wrapper'>
+        <div className='container'>
+            <div className='card'>
+                <div className='card-header'>
+                    <image></image>
+                    <div className='card-title'>Nossas prioridades</div>
+                </div>
+                <div className='card-text'>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
+                </div>
+                <button className='card-button'></button>
+            </div>
+            <div className='card'>
+                <image></image>
+                <div className='card-title'>Meio ambiente</div>
+            </div>
+            <div className='card'>
+                <image></image>
+                <div className='card-title'>Periferias</div>
+            </div>
+        </div>
+    </section>
+}
+
+export default problematicSection;
