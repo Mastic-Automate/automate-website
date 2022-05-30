699b307a64fc045d14d32d4dea88ee74c04d367b
Victor <victor.liracarlos@gmail.com>
2022-05-30 20:15:21 -0300

    Added productSection

--- src/components/pages/LandingPage/index.js
+++ src/components/pages/LandingPage/index.js
@@ -4,6 +4,7 @@ import AutomateFooter from "../../AutomateFooter";
 import Hero from "../../Hero";
 import RegularSection from "../../RegularSection";
 import StickyHeader from "../../StickyHeader";
+import ProductSection from '../../ProductSection';
 
 function LandingPage() {
     return (
@@ -15,6 +16,8 @@ function LandingPage() {
             <RegularSection imgSide="right" src="/img/config-screen.png" alt="imagem do celular na tela de sincronização" title="Fácil de configurar" text={<p>Automatize o crescimento da sua planta</p>} />
 
             <RegularSection imgSide="left" src="/img/monitoring-screen.png" alt="imagem do celular na tela de monitoramento" title="Cuidados especiais" text={<p>Monitore sua planta constantemente</p>} />
+
+            <ProductSection links={[{link: '/', linkName:'Nosso Produto'}]}/>
 
             <AutomateFooter />
         </div>
