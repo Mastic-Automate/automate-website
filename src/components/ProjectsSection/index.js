9ca4b0855a13dc82a9bc0b8597027c84dd56e62a
YanMCoutinho <yan.m.coutinho@gmail.com>
2022-06-06 09:29:24 -0300

    Changed projects section -not responsive

--- src/components/ProjectsSection/index.js
+++ src/components/ProjectsSection/index.js
@@ -4,50 +4,47 @@ import './ProjectsSection.css';
 import enterpriseLogo from '../../img/logo-mastic-removedBG.png';
 
 function ProjectsSection() {
-  return (
-    <div className='projects-wrapper'>
-        <div className='card-projects'>
-            <h1 className='card-projects-title'>Projetos</h1>
-            <img src={ enterpriseLogo } alt="logo-mastic" className="logo-mastic"></img>
-        </div>
-        <div className='carousel-wrapper'>
-            <div className='carousel'>
-              <div className='card'>
-                <div className='card-header'>
-
-                </div>
-                <div className='card-body'>
-
-                </div>
-
-              </div>
-              <div className='card active'>
-                <div className='card-header'>
-
-                </div>
-                <div className='card-body'>
-
-                </div>
-                
-              </div>
-              <div className='card'>
-                <div className='card-header'>
-
-                </div>
-                <div className='card-body'>
-
-                </div>
-                
-              </div>
-            </div>
-              
-
-            <button className='btn-primary'>Ver projetos</button>
-        </div>
-        
-        
+return (
+  <div className='projects-wrapper'>
+    <div className='card-projects'>
+      <h1 className='card-projects-title'>Projetos</h1>
+      <div className='row'>
+        <div className='line'></div>
+        <img src={ enterpriseLogo } alt="logo-mastic" className="logo-mastic" />
+        <div className='line'></div>
+      </div>
     </div>
-  );
+    <div className='carousel-wrapper'>
+      <div className='carousel'>
+        
+        <div className='card'>
+          <div className='card-header'>
+          </div>
+          <div className='card-body'>
+          </div>
+        </div>
+
+        <div className='card active'>
+          <div className='card-header'>
+          </div>
+
+          <div className='card-body'>
+          </div>
+
+        </div>
+
+        <div className='card'>
+          <div className='card-header'>
+          </div>
+          <div className='card-body'>
+          </div>
+        </div>
+
+      </div>
+      <button className='btn-primary'>Ver projetos</button>
+    </div>
+  </div>
+);
 }
 
 export default ProjectsSection;
