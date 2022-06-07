e2ec5e270357dc452659623d4b64590105a1889b
vicLira <victor.liracarlos@gmail.com>
2022-06-07 07:44:35 -0300

    Create global theme

---
+++ src/global/theme/index.js
@@ -0,0 +1,18 @@
+export const theme = {
+    COLORS: {
+        BG_COLOR: "#E5E5E5",
+        SECONDARY_BG_COLOR: "#CACACA",
+
+        PRIMARY: "#1EEC58",
+        GREEN_900: "#123C00",
+        GREEN_800: "#407A26",
+        GREEN_700: "#267B00",
+        SECONDARY_COLOR: "#161722",
+        BLUE_900: "#242434",
+
+        ERROR: "#FF5959",
+        WHITE: "#FFFFFF",
+    }
+
+    
+}
