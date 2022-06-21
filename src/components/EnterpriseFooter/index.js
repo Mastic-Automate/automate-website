6c0641f1de4f1fa864c8e06d625d9cd3f2e490ae
vicLira <victor.liracarlos@gmail.com>
2022-06-21 10:36:51 -0300

    Update footer to styled components

--- src/components/EnterpriseFooter/index.js
+++ src/components/EnterpriseFooter/index.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import enterpriseLogo from '../../img/logo-mastic-removedBG.png';
-import './EnterpriseFooter.css'
 
-import { FooterWrapper, Container, OurPriorities, ContainerPriorities, BGGreen } from './styles';
+import { FooterWrapper, Container, OurPriorities, ContainerPriorities, BGGreen, Title, Projects, Project, FastLinks } from './styles';
 
 export default function EnterpriseFooter() {
     return (
@@ -11,37 +10,37 @@ export default function EnterpriseFooter() {
             <Container>
                 <OurPriorities className='our-priorities'>
                     <ContainerPriorities>
-                        <div className='title'>
+                        <Title className='title'>
                             <img src={ enterpriseLogo } alt='Logo da Mastic' />
                             <h2>Nossas Prioridades</h2>
-                        </div>
+                        </Title>
                         
                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
                     </ContainerPriorities>
                 </OurPriorities>
-                <div className='projects'>
+                <Projects className='projects'>
                     <h2>Projetos</h2>
-                    <div className='project'>
+                    <Project className='project'>
                         <div className='img'></div>
                         <h3>AUTOMATE</h3>
-                    </div>
-                    <div className='project'>
+                    </Project>
+                    <Project className='project'>
                         <div className='img'></div>
                         <h3>AUTOMATE</h3>
-                    </div>
-                    <div className='project'>
+                    </Project>
+                    <Project className='project'>
                         <div className='img'></div>
                         <h3>AUTOMATE</h3>
-                    </div>                
-                </div>
-                <div className='fast-links'>
+                    </Project>                
+                </Projects>
+                <FastLinks className='fast-links'>
                     <h2>Links Rápidos</h2>
                     <ul>
                         <li>HOME</li>
                         <li>Produtos</li>
                         <li>sobre nós</li>
                     </ul>
-                </div>
+                </FastLinks>
             </Container>
             
         </FooterWrapper>
--- src/components/EnterpriseFooter/styles.js
+++ src/components/EnterpriseFooter/styles.js
@@ -36,7 +36,7 @@ export const Container = styled.div`
     height: 100%;
     margin: 0 auto;
     display: flex;
-    justify-content: space-between;
+    justify-content: flex-end;
     align-items: center;
     z-index: 2;
     
@@ -51,6 +51,10 @@ export const OurPriorities = styled.div`
     background-image: linear-gradient(to left,#42702E, #4F8735);
     left: 0;
     bottom: 0;
+
+    & p { 
+        text-align: justify;
+    }
 `;
 
     export const ContainerPriorities = styled.div`
@@ -59,3 +63,76 @@ export const OurPriorities = styled.div`
         display: flex;
         flex-direction: column;
     `;
+
+export const Title = styled.div`
+    display: flex;
+    text-align: center;
+    align-items: center;
+    justify-content: space-between;
+
+    & img {
+        max-width: 85px;
+    }
+
+    & h2 {
+        width: 60%;
+        margin: auto;
+        text-align: left;
+        border-left: 1px solid white;
+        padding-left: 2rem;
+    }
+
+`;
+
+export const Projects = styled.div`
+    width: 20%;
+    padding-left: 6rem;
+    min-width: 320px;
+`;
+
+export const Project = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    margin-top: 2rem;
+
+    &:first-child() {
+        margin-top: 1rem;
+    }
+
+    & h3 {
+        font-family: 'Oswald-Light';
+        font-weight: 400;
+    }
+
+    & img {
+        width: 40px;
+        height: 40px;
+        background-color: #CACACA;
+        margin-right: 10px;
+    }
+    
+`;
+
+export const FastLinks = styled.div`
+    width: 48%;
+    text-align: left;
+    margin-left: 2rem;
+
+    & h2 {
+        width: 100%;
+    }
+
+    & ul {
+        margin-bottom: 0;
+    }
+
+    & ul li {
+        font-family: Oswald-Light;
+        list-style-type: square;
+        list-style-position: inside;
+        text-transform: uppercase;
+        margin-top: 2rem;
+        margin-left: 0.4rem;
+    }
+`;
