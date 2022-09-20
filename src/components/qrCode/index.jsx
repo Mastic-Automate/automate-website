52858de6c026d8f3f9ab7d73f2f38900cf44f060
Victor Lira <victor.liracarlos@gmail.com>
2022-09-20 18:45:01 +0000

    WIP dev-pix

--- src/components/qrCode/index.jsx
+++ src/components/qrCode/index.jsx
@@ -1,6 +1,4 @@
-import { dinamicPix, staticPix } from "pix-charge";
-import QRCode from "qrcode";
-
+import { QRCodePayload } from 'brpix-api-node'
 import styled from "styled-components";
 
 const QrCodeWrapper = styled.div`
@@ -8,35 +6,38 @@ const QrCodeWrapper = styled.div`
     position: absolute;
     top: 0;
 
-    #canvas {
-        z-index: 99;
-
-    }
 `;
 
 function QrCode() {
 
-    window.onload = function ({merchantKey, merchantName, amount}) {
-      let brCode = staticPix({
-        merchantKey: {merchantKey},
-        merchantName: {merchantName},
-        amount: {amount},
-      });
-      
-      QRCode.toCanvas(document.getElementById("canvas"), brCode, (error) => {
-        if (error) {
-          console.error(error);
-        } else {
-          console.log(brCode);
-        }
-      });
+    window.onload = async function() {
+      // instanciando QRCode
+    const codePayload = new QRCodePayload({
+      isStatic: true, // importante sinalizar que é pix estático
+      txid: '***', // no pix estático será preenchido com '***' automaticamente
+      pixKey: import.meta.env.VITE_PIX_KEY,
+      amount: import.meta.env.AMOUNT,
+      merchantCity: import.meta.env.MERCHANT_CITY,
+      merchantName: import.meta.env.MERCHANT_NAME,
+      uniquePayment: false, // true se deve ser usado apenas uma vez
+      // description: 'Descricao do pagamento'
+    })
+
+      // pode adquirir a string do QRCode
+      console.log('string payload', codePayload.getPayload()) 
+
+      // adquirir string base64 para imagem QRCode
+      const strBase64 = await codePayload.getQRCode({ width: 400 })
+
+      const qrCode = document.getElementById(qrCode).src = {strBase64};
+
     }
-    
+  
   
     return (
       
       <QrCodeWrapper>
-        <canvas id="canvas" width='500' height='500'></canvas>
+        <img id="qrCode" />
       </QrCodeWrapper>
     )
   }
